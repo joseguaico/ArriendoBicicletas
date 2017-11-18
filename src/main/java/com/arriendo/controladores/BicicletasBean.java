27df28a7f9ac7610f1337d9bbf494861056823b1
Jose Guaico Astorga <joseguaico@gmail.com>
2017-11-18 15:46:36 -0300

    actualizo archivos locales desde cambios aplicados a origin

--- src/main/java/com/arriendo/controladores/BicicletasBean.java
+++ src/main/java/com/arriendo/controladores/BicicletasBean.java
@@ -9,7 +9,6 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
-import javax.faces.view.ViewScoped;
 import org.primefaces.context.RequestContext;
 
 @SessionScoped
